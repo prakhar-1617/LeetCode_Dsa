5d3a9ae2714fe16b205696ac1773db010852507a
Prakhar Kumar <prakharkumarcse@gmail.com>
2026-06-22 17:04:14 +0530

    Time: 55 ms (42.3%), Space: 70.2 MB (72.7%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int totalFruit(int[] fruits) {
+   //  question saying to //Find the longest contiguous subarray containing at most 2 distinct elements.
+    HashMap<Integer,Integer> map=new HashMap<>();
+    int max=0;
+      int left=0;
+      int len=0;
+      for(int right=0;right<fruits.length;right++){
+        //expanding hashmap
+        map.put(fruits[right],map.getOrDefault(fruits[right],0)+1);
+
+        //if invalid shrink the hashmao
+        while(map.size()>2){
+            map.put(fruits[left],map.get(fruits[left])-1);
+            if(map.get(fruits[left])==0){
+                map.remove(fruits[left]);
+            }
+            left++;
+        }
+
+        if(map.size()<=2){
+            len=right-left+1;
+            max=Math.max(max,len);
+        }
+
+
+      } 
+      return max;
+    }
+}
