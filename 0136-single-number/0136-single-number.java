0bd62a9c3f1149dfeaeaecce7a6c8798b24741af
Prakhar Kumar <prakharkumarcse@gmail.com>
2026-06-30 13:29:38 +0530

    Time: 14 ms (14.86%), Space: 47.1 MB (27.68%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+       HashMap<Integer,Integer> map=new HashMap<>();
+       for(int n:nums){
+        map.put(n,map.getOrDefault(n,0)+1);
+       } 
+       for(int key:map.keySet()){
+        if(map.get(key)==1){
+            return key;
+        }
+       }
+       return 0;
+    }
+}
