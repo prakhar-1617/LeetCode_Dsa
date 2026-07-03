fd170785db0412afc75b1657f37c54be142a1d4d
Prakhar Kumar <prakharkumarcse@gmail.com>
2026-07-03 13:13:29 +0530

    Time: 0 ms (100%), Space: 43.6 MB (90.31%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -3,9 +3,9 @@ class Solution {
        //usung binary search for finding targer
        int m=matrix.length;
        int n=matrix[0].length;
-       if(m==1 && n==1 && matrix[0][0]==target){
-        return true;
-       }
+    //    if(m==1 && n==1 && matrix[0][0]==target){
+    //     return true;
+    //    }
        int i=0;
        int j=n-1;
         while(i<=m-1 && j>=0){
