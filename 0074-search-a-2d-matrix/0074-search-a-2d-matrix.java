5cf7e8fdbb37868d8ad181d0ee99b5ec97e4a4f2
Prakhar Kumar <prakharkumarcse@gmail.com>
2026-07-03 13:13:09 +0530

    Time: 0 ms (100%), Space: 43.9 MB (42.8%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+       //usung binary search for finding targer
+       int m=matrix.length;
+       int n=matrix[0].length;
+       if(m==1 && n==1 && matrix[0][0]==target){
+        return true;
+       }
+       int i=0;
+       int j=n-1;
+        while(i<=m-1 && j>=0){
+            if(matrix[i][j]==target){
+                return true;
+            }
+            else if(matrix[i][j]<target){
+                i++;
+            }
+            else{
+            j--;
+            }
+            }
+        
+        return false;
+    }
+}
