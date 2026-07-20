e03146c73bd60084b04736cca2855a66299d5ff8
Prakhar Kumar <prakharkumarcse@gmail.com>
2026-07-20 14:11:28 +0530

    Time: 1 ms (99.93%), Space: 47.7 MB (81.38%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,19 +1,15 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        //solving using greedy algo
-        int maxidx=0;
-        if(nums.length==1){
-            return true;
+
+        int goal = nums.length - 1;
+
+        for (int i = nums.length - 2; i >= 0; i--) {
+
+            if (i + nums[i] >= goal) {
+                goal = i;
+            }
         }
-        for(int i=0;i<nums.length;i++){
-           
-            if(i>maxidx){  //Agar current index hi reachable nahi hai (i > maxidx), to aage kabhi bhi nahi ja paoge.Isliye directly false.
-            return false;
-        }
-         maxidx=Math.max(nums[i]+i,maxidx);
-            
-        }
-         
-        return true;
+
+        return goal == 0;
     }
 }
